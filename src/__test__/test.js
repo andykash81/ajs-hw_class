48fe5a73591a8e59c5896732f9344eb296d1f005
andykash81 <k81@ngs.ru>
2022-07-23 00:54:15 +0300

    Initial commit

---
+++ src/__test__/test.js
@@ -0,0 +1,71 @@
+import Character from '../character';
+import Bowman from '../bowman';
+import Swordsman from '../swordsman';
+import Magician from '../magician';
+import Daemon from '../daemon';
+import Undead from '../undead';
+import Zombie from '../zombie';
+
+test('Character correct', () => {
+  const character = new Character('hero', 'Bowman');
+  const correct = {
+    name: 'hero', type: 'Bowman', health: 100, level: 1,
+  };
+  expect(character).toEqual(correct);
+});
+
+test('Name not correct', () => {
+  expect(() => new Character('h', 'Bowman')).toThrow(Error);
+});
+
+test('Type not correct', () => {
+  expect(() => new Character('hero', 'Swordsmans')).toThrow(Error);
+});
+
+test('Bowman correct', () => {
+  const bowman = new Bowman('hero', 'Bowman');
+  const correct = {
+    name: 'hero', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  };
+  expect(bowman).toEqual(correct);
+});
+
+test('Swordsman correct', () => {
+  const swordsman = new Swordsman('hero', 'Swordsman');
+  const correct = {
+    name: 'hero', type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10,
+  };
+  expect(swordsman).toEqual(correct);
+});
+
+test('Magician correct', () => {
+  const magician = new Magician('hero', 'Magician');
+  const correct = {
+    name: 'hero', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40,
+  };
+  expect(magician).toEqual(correct);
+});
+
+test('Daemon correct', () => {
+  const daemon = new Daemon('hero', 'Daemon');
+  const correct = {
+    name: 'hero', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40,
+  };
+  expect(daemon).toEqual(correct);
+});
+
+test('Undead correct', () => {
+  const undead = new Undead('hero', 'Undead');
+  const correct = {
+    name: 'hero', type: 'Undead', health: 100, level: 1, attack: 25, defence: 25,
+  };
+  expect(undead).toEqual(correct);
+});
+
+test('Zombie correct', () => {
+  const zombie = new Zombie('hero', 'Zombie');
+  const correct = {
+    name: 'hero', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10,
+  };
+  expect(zombie).toEqual(correct);
+});
---
+++ src/character.js
@@ -0,0 +1,18 @@
+const typeList = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
+export default class Character {
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Имя персонажа должно быть от 2 до 10 символов');
+    } else {
+      this.name = name;
+    }
+    if (typeList.includes(type)) {
+      this.type = type;
+    } else {
+      throw new Error('Тип персонажа не соответствует списку');
+    }
+    this.health = 100;
+    this.level = 1;
+  }
+}
